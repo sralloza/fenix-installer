3a790bca09cce14351fe7d105d6ea71c4904a3a8
Diego Alloza González <sralloza@gmail.com>
2021-09-25 21:13:53 +0200

    chore: fix lru_cache on python37

--- installer/inputs.py
+++ installer/inputs.py
@@ -10,30 +10,30 @@ from .models import Profile, Service
 yaml = YAML(typ="safe")
 
 
-@lru_cache
+@lru_cache()
 def get_yml(name: str):
     with CONFIG_DIR.joinpath(f"{name}.yml").open() as f:
         return yaml.load(f)
 
 
-@lru_cache
+@lru_cache()
 def get_json(name: str):
     with SCHEMAS_DIR.joinpath(f"{name}.schema.json").open() as f:
         return json.load(f)
 
 
-@lru_cache
+@lru_cache()
 def get_services():
     services_data = get_yml("services")
     return [Service(**x) for x in services_data]
 
 
-@lru_cache
+@lru_cache()
 def get_profiles():
     profiles_data = get_yml("profiles")
     return [Profile(**x) for x in profiles_data]
 
 
-@lru_cache
+@lru_cache()
 def get_auths() -> Dict[str, List[str]]:
     return get_yml("auths")
